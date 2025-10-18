5f320948279601eaa37264cf608ef1a1817491ea
Sophea3 <sopheakaing354@gmail.com>
2025-10-18 10:51:13 +0700

    TP1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">This is my first VueJs project</div>
+</template>
+
+<script setup lang="ts">
+// no logic needed here
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 100px;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
